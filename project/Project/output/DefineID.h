4dd470b9e0d7cb524be04c1ccb404dbac53d9a73
bigym <595335057@qq.com>
2020-06-30 13:37:14 +0800

    backups

---
+++ project/Project/output/DefineID.h
@@ -0,0 +1,45 @@
+//创建画面开机的命令索引ID
+#define  _SCREEN_开机                                                            0
+
+//创建画面导航的命令索引ID
+#define  _SCREEN_导航                                                            1
+
+//创建画面d33测试的命令索引ID
+#define  _SCREEN_D33测试                                                         2
+
+//创建画面报表的命令索引ID
+#define  _SCREEN_报表                                                            3
+
+//创建画面系统设置的命令索引ID
+#define  _SCREEN_系统设置                                                          4
+
+#define  _BTN_开机_BTN_TEST                                                      2
+
+#define  _BTN_导航_D33                                                           2
+
+#define  _BTN_导航_D31                                                           3
+
+#define  _BTN_导航_D15                                                           4
+
+#define  _BTN_导航_BUTTON2                                                       1
+
+#define  _BTN_D33测试_BUTTON1                                                    9
+
+#define  _BTN_D33测试_BUTTON2                                                   10
+
+#define  _RECORD_报表_RECORD1                                                    1
+
+#define  _BTN_报表_BUTTON1                                                       3
+
+//画面报表中按钮Button2弹起时的图片
+#define  _IMG_报表_BUTTON2_UP                                                    2
+
+#define  _BTN_报表_BUTTON2                                                       4
+
+#define  _BTN_系统设置_BUTTON1                                                     3
+
+//画面系统设置中按钮Button2弹起时的图片
+#define  _IMG_系统设置_BUTTON2_UP                                                  2
+
+#define  _BTN_系统设置_BUTTON2                                                     4
+
