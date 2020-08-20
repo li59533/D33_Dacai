5f61917ba6d16e8f8900745c46bcd99491d4e4df
bigym <39073582+li59533@users.noreply.github.com>
2020-08-20 19:47:50 +0800

    updata
    1.大彩屏

---
+++ project/output/DefineID.h
@@ -0,0 +1,82 @@
+//创建画面画面1的命令索引ID
+#define  _SCREEN_画面1                                                           0
+
+//创建画面画面2的命令索引ID
+#define  _SCREEN_画面2                                                           1
+
+//创建画面启动页的命令索引ID
+#define  _SCREEN_启动页                                                           2
+
+//创建画面首页的命令索引ID
+#define  _SCREEN_首页                                                            3
+
+//创建画面D值的命令索引ID
+#define  _SCREEN_D值                                                            4
+
+//创建画面C值的命令索引ID
+#define  _SCREEN_C值                                                            5
+
+//创建画面设置的命令索引ID
+#define  _SCREEN_设置                                                            6
+
+#define  _BTN_画面1_BUTTON1                                                      2
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY1                                           3
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY2                                           4
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY3                                           5
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY4                                           6
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY5                                           7
+
+#define  _TXT_DIS__画面1_TEXT_DISPLAY6                                           8
+
+#define  _SLIDER_画面1_SLIDER1                                                   9
+
+#define  _BTN_画面1_BUTTON2                                                     16
+
+#define  _BTN_画面1_BUTTON3                                                     17
+
+#define  _BTN_画面2_BUTTON1                                                      2
+
+#define  _BTN_首页_BUTTON1                                                       1
+
+//全局图片首页2.png
+#define  _IMG_首页2_PNG                                                          4
+
+#define  _BTN_首页_BUTTON2                                                       2
+
+#define  _BTN_首页_BUTTON3                                                       3
+
+#define  _BTN_首页_BUTTON4                                                       4
+
+#define  _BTN_首页_BUTTON5                                                       5
+
+#define  _TXT_DIS__D值_TEXT_DISPLAY1                                            5
+
+#define  _TXT_DIS__D值_TEXT_DISPLAY2                                            6
+
+#define  _TXT_DIS__D值_TEXT_DISPLAY3                                            7
+
+#define  _BTN_D值_BUTTON1                                                       8
+
+//全局图片d值2.png
+#define  _IMG_D值2_PNG                                                          6
+
+#define  _BTN_D值_BUTTON2                                                       9
+
+#define  _TXT_DIS__C值_TEXT_DISPLAY1                                            5
+
+#define  _TXT_DIS__C值_TEXT_DISPLAY3                                            7
+
+#define  _BTN_C值_BUTTON1                                                       3
+
+//全局图片c值2.png
+#define  _IMG_C值2_PNG                                                          8
+
+#define  _BTN_C值_BUTTON2                                                       6
+
+#define  _BTN_设置_BUTTON1                                                       1
+
